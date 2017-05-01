24a4cdcaa2d7b6dcad46cebf03512f969ec87244
Jethro Muller <git@jethromuller.co.za>
2017-05-01 21:38:44 +0200

    Add views to show episode list and hide series

--- ordering/list/routes.py
+++ ordering/list/routes.py
@@ -1,8 +1,31 @@
 from sanic import Blueprint
 
-from .views import Index
+from .views import Index, IndexWithHiddenSeries
 from ..constants import GET, POST
 
 
 episode_list = Blueprint(name='list')
-episode_list.add_route(Index.as_view(), uri='/', methods=[GET])
+
+episode_list.add_route(
+    Index.as_view(),
+    uri='/',
+    methods=[GET],
+)
+
+episode_list.add_route(
+    Index.as_view(),
+    uri='/<newest_first>',
+    methods=[GET],
+)
+
+episode_list.add_route(
+    IndexWithHiddenSeries.as_view(),
+    uri='/hide/<hide_list:(\w+\+?)+>/',
+    methods=[GET],
+)
+
+episode_list.add_route(
+    IndexWithHiddenSeries.as_view(),
+    uri='/hide/<hide_list:(\w+\+?)+>/<newest_first>',
+    methods=[GET],
+)
--- ordering/list/views.py
+++ ordering/list/views.py
@@ -1,39 +1,33 @@
 from sanic.views import HTTPMethodView
 
 from .. import render
+from ..utils import get_full_series_episode_list
 
 
 class Index(HTTPMethodView):
-    async def get(self, request):
-        return render('index.html', request, title='ABC')
+    async def get(self, request, newest_first=None):
+        episode_list = get_full_series_episode_list()
+
+        if newest_first == 'newest_first':
+            episode_list = episode_list[::-1]
+
+        context = {
+            'table_content': episode_list,
+        }
+
+        return render('index.html', request, **context)
 
 
-# @app.route('/', methods=['GET'])
-# @app.route('/<newest_first>', methods=['GET'])
-# def index(newest_first=None):
-#     context = {}
-#
-#     episode_list = get_full_series_episode_list()
-#
-#     if newest_first == 'newest_first':
-#         episode_list = episode_list[::-1]
-#
-#     context['table_content'] = episode_list
-#
-#     return app.jinja.render_template('index.html', **context)
-#
-#
-# @app.route('/hide/<list:hide_list>/', methods=['GET'])
-# @app.route('/hide/<list:hide_list>/<newest_first>', methods=['GET'])
-# def index_with_hidden(hide_list, newest_first=None):
-#     context = {}
-#
-#     episode_list = get_full_series_episode_list(hide_list)
-#
-#     if newest_first == 'newest_first':
-#         episode_list = episode_list[::-1]
-#
-#     context['hidden_list'] = hide_list
-#     context['table_content'] = episode_list
-#
-#     return app.jinja.render_template('index.html', **context)
+class IndexWithHiddenSeries(HTTPMethodView):
+    async def get(self, request, hide_list, newest_first=None):
+        episode_list = get_full_series_episode_list(hide_list)
+
+        if newest_first == 'newest_first':
+            episode_list = episode_list[::-1]
+
+        context = {
+            'hidden_list': hide_list,
+            'table_content': episode_list,
+        }
+
+        return render('index.html', request, **context)
